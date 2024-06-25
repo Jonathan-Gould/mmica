3eb0ab112029bb39e5694025eb18f2fb27a66664
Jonathan Gould <jonathan.d.gould@gmail.com>
2024-06-25 17:02:57 -0400

    adds a python version of the conjugate gradient

--- mmica/_utils.py
+++ mmica/_utils.py
@@ -5,7 +5,44 @@ import os
 
 import numpy as np
 
-from ._conjugate_gradient import cg_c
+from ._conjugate_gradient import cg_c as cython_cg_c
+
+def python_cg_c(B: np.ndarray, i: int, max_iter: int, tol: float, N: int):
+
+    x = np.zeros(N)
+    B_diag = np.empty(N)
+    r = np.empty(N)
+    y = np.empty(N)
+    p = np.empty(N)
+    Ap = np.empty(N)
+
+    B_diag = np.diag(B)
+    r = B[i,:]
+    inv_bii = 1. / B[i, i]
+    r = r * inv_bii
+    x[i] = inv_bii
+    r[i] = 0.
+    for j in range(N):
+        y[j] = r[j] / B_diag[j]
+    p = y
+    prod_old = r @ y
+    for n in range(max_iter):
+        Ap = B @ p
+        alpha = prod_old / (p @ Ap)
+        minus_alpha = - alpha
+        x = x +  minus_alpha * p
+        r = r + Ap * minus_alpha
+        norm = np.linalg.norm(r)
+        if norm < tol:
+            break
+        for j in range(N):
+            y[j] = r[j] / B_diag[j]
+        prod_new = r@y
+        beta = prod_new / prod_old
+        prod_old = prod_new
+        for j in range(N):
+            p[j] = y[j] + beta * p[j]
+    return x
 
 
 def amari_d(W, A):
@@ -31,8 +68,9 @@ def whitening(Y, mode='sph'):
     return Z, W
 
 
-def cg(B, i, max_iter=10, tol=1e-10):
+def cg(B, i, max_iter=10, tol=1e-10, use_cython=True):
     '''
     Wrapper to call Cython
     '''
+    cg_c = cython_cg_c if use_cython else python_cg_c
     return cg_c(B, i, max_iter, tol, B.shape[0])
